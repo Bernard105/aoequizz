01d7b88ae3e2f48fb6f9191d5c4619b37d4c9d60
Trần Mạnh Tài <101389458+taidev105@users.noreply.github.com>
2022-07-04 09:52:48 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,87 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+var questions = [
+    new Question("Nước nào cung được thêm 1 dame?", ["Hittle", "Assyrian","Mioan", "Greek"], "Hittle"),
+    new Question("Y nước nào trâu nhất?", ["Greek", "Macedonia", "Cathage", "Roma"], "Cathage"),
+    new Question("Nước nào dân 35 thực?", ["Parmyral", "Shang","Greek", "Choson"], "Shang"),
+    new Question("Nước nào có pháo bẹp bắn nhanh nhất?", ["Hittle", "Macedonia", "", "Sumerian"], "Summerian"),
+    new Question("Nước nào ăn hươu voi nhanh nhất?", ["Parmyral", "Shang", "Assyrian", "Persian"], "Persian")
+];
+ 
+var quiz = new Quiz(questions);
+
+populate();
